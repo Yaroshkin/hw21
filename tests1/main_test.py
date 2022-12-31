8ac0317b30c190b2715e81e8a5e57a8d8e310804
yaroshkin <yaroshkin255@ukr.net>
2022-12-31 11:42:23 +0200

    add class min and max num

--- tests1/main_test.py
+++ tests1/main_test.py
@@ -11,3 +11,13 @@ class MyClassTest(unittest.TestCase):
         num = main.Numbers(1,2,3,4)
         result = num.sredNum()
         self.assertEqual(2.5,result)
+
+    def test_max(self):
+        num = main.Numbers(1,2,3,4)
+        result = num.maxNum()
+        self.assertEqual(4,result)
+
+    def test_min(self):
+        num = main.Numbers(1,2,3,4)
+        result = num.minNum()
+        self.assertEqual(1,result)
