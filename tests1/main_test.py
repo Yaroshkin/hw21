dcd8e4b38116274ffd1b2c8ab779e2835d63d66c
yaroshkin <yaroshkin255@ukr.net>
2023-01-05 17:15:22 +0200

    add testing

--- tests1/main_test.py
+++ tests1/main_test.py
@@ -21,3 +21,24 @@ class MyClassTest(unittest.TestCase):
         num = main.Numbers(1,2,3,4)
         result = num.minNum()
         self.assertEqual(1,result)
+
+    def test_sum(self):
+        num = main.Numbers(10,15,25,40)
+        result = num.sumNum()
+        self.assertEqual(90, result)
+
+    def test_sred(self):
+        num = main.Numbers(100,200,300,400)
+        result = num.sredNum()
+        self.assertEqual(250,result)
+
+    def test_max(self):
+        num = main.Numbers(100,2000,323,4685)
+        result = num.maxNum()
+        self.assertEqual(4685,result)
+
+    def test_min(self):
+        num = main.Numbers(1,223,320,4589)
+        result = num.minNum()
+        self.assertEqual(1,result)
+
